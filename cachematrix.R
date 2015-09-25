7b0a590279b3748be2c497b6a4b747f4e0ec36c6
ryanivanka <sarah.hhyy@gmail.com>
2015-09-25 16:22:57 +0800

    submit assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,65 @@
-## Put comments here that give an overall description of what your
-## functions do
+## cachematrix.R
+##
+## Create a cache marix object that can be used to
+## repeatably solve the inverse of the marix, but only
+## calculates the inverse once.
+##
+## Usage:
+##  M <- matrix(1:16, nrow=4, ncol=4)
+##  matrixStorage <- makeCacheMatrix()$set(M)
+##  cacheSolve(matrixStorage)
+##  matrixStorage$getInverse() # Return the inverse matrix
+##
+##  N <- matrix(c(1, 2, 3, 4), nrow=2, ncol=2)
+##  matrixStorage$set(N)      # Set new matrix, clean the cache
+##  matrixStorage$get()  # Returns the matrix being cached, it's N
+##  matrixStorage$getInverse() # Return null
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function() {
+  x <- NULL
+  cachedInverse <- NULL
+  
+  ## Original matrix setter and getter,  inverse matrix reset to null
+  set <- function(y) {
+    if (is.matrix(y) && nrow(y) == ncol(y) && det(y)) {
+      ## cache it
+      x <<- y
+      cachedInverse <<- NULL    
+    } else {
+      stop("Validation Error: This matrix is not a squared matrix. It cannot be inversed.")
+    }
+  }
+  get <- function() x
+  
+  ## Inverse matrix setter and getter
+  setInverse <- function(inverse) cachedInverse <<- inverse
+  getInverse <- function() cachedInverse
+  
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## Return the inverse of an cacheMatrix object
+cacheSolve <- function(makeCacheMatrixInterface, ...) {
+  ## Return a matrix that is the inverse of 'x'
+  inverseMatrix <- makeCacheMatrixInterface$getInverse()
+  
+  ## Check if 
+  if(!is.null(inverseMatrix)) {
+    message("Use cached inverse matrix.")
+    return (invFunc)
+  }
+  
+  ## Calculate inverse matrix
+  originalMatrix <- makeCacheMatrixInterface$get()
+  inverseMatrix <- solve(originalMatrix)
+  
+  ## Cache inverse matrix
+  makeCacheMatrixInterface$setInverse(inverseMatrix)
+  
+  ## Return inverse matrix
+  inverseMatrix
 }
+
